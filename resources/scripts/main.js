e01a90ab3d1de72b123fdc64053cc44e6c309c7a
bjp5082 <bjp5082@gmail.com>
2018-04-24 00:26:23 -0500

    fix animation for skills section

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -2,38 +2,39 @@ $(document).ready(() => {
 
   // Skill Section Animations: look into how to refactor to reduce repitition
   $('#html').hover(function () {
-    $('#html-details').toggleClass('skills__skillsDetails--visible').animate({left: "0"}, 400);
+    $('#html-details').toggleClass('skills__skillsDetails--visible').css("left", "100%").animate({left: "0"}, 400);
   }, function() {
-    $('#html-details').css("left", "100%").toggleClass('skills__skillsDetails--visible');
-  });
+    $('#html-details').toggleClass('skills__skillsDetails--visible');
+    });
+
 
   $('#sass').hover(function () {
-    $('#sass-details').toggleClass('skills__skillsDetails--visible').animate({left: "0"}, 400);
+    $('#sass-details').toggleClass('skills__skillsDetails--visible').css("left", "100%").animate({left: "0"}, 400);
   }, function() {
-    $('#sass-details').toggleClass('skills__skillsDetails--visible').css("left", "100%");
+    $('#sass-details').toggleClass('skills__skillsDetails--visible');
     });
 
   $('#responsive').hover(function () {
-    $('#rd-details').toggleClass('skills__skillsDetails--visible').animate({left: "0"}, 400);
+    $('#rd-details').toggleClass('skills__skillsDetails--visible').css("left", "100%").animate({left: "0"}, 400);
   }, function() {
-    $('#rd-details').toggleClass('skills__skillsDetails--visible').css("left", "100%");
+    $('#rd-details').toggleClass('skills__skillsDetails--visible');
     });
 
   $('#git').hover(function () {
-    $('#git-details').toggleClass('skills__skillsDetails--visible').animate({left: "0"}, 400);
+    $('#git-details').toggleClass('skills__skillsDetails--visible').css("left", "100%").animate({left: "0"}, 400);
   }, function() {
-    $('#git-details').toggleClass('skills__skillsDetails--visible').css("left", "100%");
+    $('#git-details').toggleClass('skills__skillsDetails--visible');
     });
 
   $('#js').hover(function () {
-    $('#js-details').toggleClass('skills__skillsDetails--visible').animate({left: "0"}, 400);
+    $('#js-details').toggleClass('skills__skillsDetails--visible').css("left", "100%").animate({left: "0"}, 400);
   }, function() {
-    $('#js-details').toggleClass('skills__skillsDetails--visible').css("left", "100%");
+    $('#js-details').toggleClass('skills__skillsDetails--visible');
     });
 
   $('#jquery').hover(function () {
-    $('#jquery-details').toggleClass('skills__skillsDetails--visible').animate({left: "0"}, 400);
+    $('#jquery-details').toggleClass('skills__skillsDetails--visible').css("left", "100%").animate({left: "0"}, 400);
   }, function() {
-    $('#jquery-details').toggleClass('skills__skillsDetails--visible').css("left", "100%");
+    $('#jquery-details').toggleClass('skills__skillsDetails--visible');
     });
 });
