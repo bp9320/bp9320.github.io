bf6856e14546f612248879e51e6b2758ca2b7d5b
bjp5082 <bjp5082@gmail.com>
2018-04-27 16:44:07 -0500

    refactor/fix code for skills animations. change skills CSS for animation

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,73 +1,30 @@
 $(document).ready(() => {
-
-  function skillsQuery(x) {
-    if(x.matches) {
-      // Skill Section Animations when screen width <= 690px: look into how to refactor to reduce repetition
-      $('#html').hover(function () {
-        $('#html-details').slideToggle();
-        });
-
-      $('#sass').hover(function () {
-        $('#sass-details').slideToggle();
-        });
-
-      $('#responsive').hover(function () {
-        $('#rd-details').slideToggle();
-        });
-
-      $('#git').hover(function () {
-        $('#git-details').slideToggle();
-        });
-
-      $('#js').hover(function () {
-        $('#js-details').slideToggle();
-        });
-
-      $('#jquery').hover(function () {
-        $('#jquery-details').slideToggle();
-        });
-
-    } else {
-
-      // Skill Section Animations when screen width > 690px: look into how to refactor to reduce repetition
-      $('#html').hover(function () {
-        $('#html-details').toggleClass('skills__skillsDetails--visible').css("left", "100%").animate({left: "0"}, 400);
-      }, function() {
-        $('#html-details').toggleClass('skills__skillsDetails--visible');
-        });
-
-      $('#sass').hover(function () {
-        $('#sass-details').toggleClass('skills__skillsDetails--visible').css("left", "100%").animate({left: "0"}, 400);
-      }, function() {
-        $('#sass-details').toggleClass('skills__skillsDetails--visible');
-        });
-
-      $('#responsive').hover(function () {
-        $('#rd-details').toggleClass('skills__skillsDetails--visible').css("left", "100%").animate({left: "0"}, 400);
-      }, function() {
-        $('#rd-details').toggleClass('skills__skillsDetails--visible');
-        });
-
-      $('#git').hover(function () {
-        $('#git-details').toggleClass('skills__skillsDetails--visible').css("left", "100%").animate({left: "0"}, 400);
-      }, function() {
-        $('#git-details').toggleClass('skills__skillsDetails--visible');
-        });
-
-      $('#js').hover(function () {
-        $('#js-details').toggleClass('skills__skillsDetails--visible').css("left", "100%").animate({left: "0"}, 400);
-      }, function() {
-        $('#js-details').toggleClass('skills__skillsDetails--visible');
-        });
-
-      $('#jquery').hover(function () {
-        $('#jquery-details').toggleClass('skills__skillsDetails--visible').css("left", "100%").animate({left: "0"}, 400);
-      }, function() {
-        $('#jquery-details').toggleClass('skills__skillsDetails--visible');
-        });
-    }
-  }
+  $('.skills__skillsDetails').hide();
   var x = window.matchMedia('(max-width: 690px)');
-  skillsQuery(x);
-  x.addListener(skillsQuery);
+
+      $('.skills__skill').hover(function() {
+
+        var id = $(this).attr('id');
+        if (x.matches) {
+          //Animation for mouseEnter when screen <= 690px;
+          $('.skills__skillsDetails').hide();
+          $('#' + id + '-details').slideDown();
+        } else {
+          // Animation for mouseEnter when screen > 690px;
+          $('.skills__skillsDetails').hide();
+          $('#' + id + '-details').show().css("left", "100%").animate({left: "0"}, 400);
+        }
+      }, function() {
+        var id = $(this).attr('id');
+        if(x.matches) {
+          //Animation for mouseLeave when screen <= 690px;
+          $('#' + id + '-details').slideUp();
+        } else {
+          // Animation for mouseLeave when screen > 690px;
+          $('#' + id + '-details').hide();
+        }
+      });
+
 });
+
+// Huge shout out to @blunket in the devICT slack workspace for the help on debugging the skillsDetails animations
